10178aa45098cfbac9971c3156fe1b2fc0c0d3b3
Ruben Tanahara <tanaharaguy@gmail.com>
2022-11-07 08:36:52 -0800

    First commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View style={styles.container}>
+        {' '}
+        Hello React - native with typescript
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
